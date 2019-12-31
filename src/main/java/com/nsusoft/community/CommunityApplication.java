07b93e2917cbcf3c7cd2ee3eb5744c1b5719b165
SpecialCare <1223071517@qq.com>
2019-12-31 18:02:17 +0800

    frist commit

---
+++ src/main/java/com/nsusoft/community/CommunityApplication.java
@@ -0,0 +1,13 @@
+package com.nsusoft.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityApplication.class, args);
+    }
+
+}
